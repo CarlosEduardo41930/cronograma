156123e300d0826c6cc38a146063f72a8c5e91b8
unknown <37066@fiecalunos.lan>
2026-04-10 10:32:39 -0300

    Adicionar charset utf8mb4

--- server/config/db.js
+++ server/config/db.js
@@ -6,6 +6,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || 'if0_41558661',
   password: process.env.DB_PASSWORD || 'kfPuSJgwumRDMTF',
   database: process.env.DB_NAME || 'if0_41558661_cronograma',
+  charset: 'utf8mb4',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
